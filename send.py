89a3ed277d59fec4bea81ce19510dbaa0e45ff2d
kaivalya1061 <33483222+kaivalya1061@users.noreply.github.com>
2018-07-26 18:04:02 +0530

    removed extra data

--- send.py
+++ send.py
@@ -10,7 +10,7 @@ channel.queue_declare(queue='hello')
 
 #sending message to queue
 #a default exchange identified by an empty string
-message = {'email':'xujxTVmX6105RtSiZBAzhnAn4iJabqaEysSDNRnI6YoMR0pel6/3hod94fL0E1xm','client':1,'remove_client':False,'domain':'2'}
+
 message = {'email':'k@g','name':'kg'}
 
 channel.basic_publish(exchange='',
